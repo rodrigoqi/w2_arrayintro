8b52e913129dbe2c74d8053ddbcd5bc1f372eec7
Rodrigo Barreto <rodrigo.barreto@qi.edu.br>
2020-09-29 08:06:37 -0300

    Exemplo básico de Arrays - inicial

---
+++ auxiliar.php
@@ -0,0 +1,16 @@
+<?php
+    function getFrutas(){
+        $frutas = array("banana",
+            "abacate",
+            "uva",
+            "abacaxi",
+            "morango",
+            "bergamota",
+            "pitanga",
+            "caqui",
+            "caju",
+            "cereja"
+        );
+        return $frutas;
+    }
+?>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<!--Tags básicas do head-->
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Arrays em PHP</title>
+	<!--Link dos nossos arquivos CSS e JS padrão-->
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<script src="js/scripts.js"></script>
+	<!--Link dos arquivos CSS e JS do Bootstrap-->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+	<div class="container">
+		<div class="row text-center">
+			<div class="col-md-12">
+				<h1>Arrays em PHP</h1>
+				<h2>Miscelânea de exemplos básicos de uso</h2>
+			</div>
+		</div>
+		<br>
+		<div class="row" id="fundo">
+			
+			<div class="col-md-12 text-center">
+				<br>
+
+				<?php
+					
+					
+
+
+				?>
+			
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
